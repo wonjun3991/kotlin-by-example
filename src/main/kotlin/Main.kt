6a60904a316fbb518ffbb77758d128269cf8f738
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 16:59:42 +0900

    functional-high-order-functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
-fun main(args: Array<String>) {
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {  // 1
+    return operation(x, y)                                          // 2
+}
 
+fun sum(x: Int, y: Int) = x + y                                     // 3
+fun mul(x: Int, y: Int) = x * y
+
+fun operation(): (Int) -> Int {                                     // 1
+    return ::square
+}
+
+fun square(x: Int) = x * x
+
+fun main() {
+    val sumResult = calculate(4, 5) { x, y -> x + y }                          // 4
+    val mulResult = calculate(4, 5, ::mul)              // 5
+    println("sumResult $sumResult, mulResult $mulResult")
+
+    val func = operation()                                          // 3
+    println(func(2))
 }
