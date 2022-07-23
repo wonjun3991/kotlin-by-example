7797e7198eb0e76eae94471b61ae70b93f431c5e
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 15:40:14 +0900

    special-classes-sealed-classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
-fun main(args: Array<String>) {
+sealed class Mammal(val name: String)                                                   // 1
 
+class Cat(private val catName: String) : Mammal(catName)                                        // 2
+class Human(private val humanName: String, val job: String) : Mammal(humanName)
+
+fun greetMammal(mammal: Mammal): String {
+    return when (mammal) {                                                                     // 3
+        is Human -> "Hello ${mammal.name}; You're working as a ${mammal.job}"    // 4
+        is Cat -> "Hello ${mammal.name}"                                         // 5
+    }                                                                                   // 6
+}
+
+fun main(args: Array<String>) {
+    println(greetMammal(Cat("Snowy")))
+    println(greetMammal(Human("Hori","Developer")))
 }
