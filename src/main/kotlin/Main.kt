9390b4229f90a89476e10b090a11ca3ba73696b9
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 13:59:31 +0900

    introduction-classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
-fun main(args: Array<String>) {
+class Customer
 
+class Contact(private val id: Int,private var email: String){
+    fun getId(): Int {
+        return this.id
+    }
+
+    fun setEmail(email: String){
+        this.email = email
+    }
+
+    fun getEmail(): String{
+        return this.email
+    }
+}
+
+fun main() {
+
+    val customer = Customer()
+
+    val contact = Contact(1, "mary@gmail.com")
+
+//    println(contact.id)
+
+    println(contact.getId())
+    println(contact.getEmail())
+    println(contact.setEmail("test@gmail.com"))
+    println(contact.getEmail())
 }
