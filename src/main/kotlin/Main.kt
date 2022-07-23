c6060e19c4b810c93ef89d77bc7023094227d915
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 14:20:05 +0900

    introduction-inheritance

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,36 @@
-fun main(args: Array<String>) {
+open class Dog {
+    open fun sayHello() {
+        println("wow wow!")
+    }
+}
 
+class Yorkshire : Dog() {
+    override fun sayHello() {
+        println("wif wif!")
+    }
+}
+
+//class YorkshireInheritance: Yorkshire(){ //Kotlin Class Default set final
+//
+//}
+
+open class Tiger(val origin: String) {
+    fun sayHello() {
+        println("A tiger from $origin says: grrhhh!")
+    }
+}
+
+class SiberianTiger : Tiger("Siberia")
+
+fun main() {
+    val dog: Dog = Dog()
+    val dogYorkshire: Dog = Yorkshire()
+    dog.sayHello()
+    dogYorkshire.sayHello()
+
+    val siberianTiger: Tiger = SiberianTiger()
+    siberianTiger.sayHello()
+
+    val tiger: Tiger = Tiger("Origin Tiger")
+    tiger.sayHello()
 }
