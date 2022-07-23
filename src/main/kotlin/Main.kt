b0a2b2026752f326b5bdda7fdc929cd9fe64a793
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 15:17:46 +0900

    special-classes-enum-classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
-fun main(args: Array<String>) {
+enum class State {
+    IDLE, RUNNING, FINISHED                           // 1
+}
 
+enum class Color(val rgb: Int) {                      // 1
+    RED(0xFF0000),                                    // 2
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00);
+
+    fun containsRed() = (this.rgb and 0xFF0000 != 0)  // this.rgb and 0xFF0000 is first operation
+}
+
+fun main(args: Array<String>) {
+    val state = State.RUNNING                         // 2
+    val message = when (state) {                      // 3
+        State.IDLE -> "It's idle"
+        State.RUNNING -> "It's running"
+        State.FINISHED -> "It's finished"
+    }
+    println(message)
+
+    val red = Color.RED
+    println(red)                                      // 4
+    println(red.containsRed())                        // 5
+    println(Color.BLUE.containsRed())                 // 6
+    println(Color.YELLOW.containsRed())               // 7
 }
