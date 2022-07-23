1e588bfdfc5692609f1433d7355f68008a098fc0
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 13:27:29 +0900

    introduction-variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
 fun main(){
-    
+    var a: String = "initial"
+    println(a)
+
+    val b: Int = 1
+    println(b)
+    val c = 3
+    println(c)
+
+    a = "testing"
+    println(a)
+//    b = 3
+//    println(b) Val cannot be reassigned
+
+    val d: Int
+
+    if (someCondition()) {
+        d = 1
+    } else {
+        d = 2
+    }
+
+    println(d)
+}
+
+fun someCondition(): Boolean {
+//    return true
+    return false
 }
