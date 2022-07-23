e1011b8c8fd751366b15f9f275422db8958e036e
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 14:43:10 +0900

    control-flow-equality-checks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>) {
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
 
+    println(authors == writers)   // 1
+    println(authors === writers)  // 2
 }
