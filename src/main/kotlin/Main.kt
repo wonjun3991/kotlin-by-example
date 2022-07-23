e3909a4763d3fa50b95a3fe27630ea151584a28e
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 13:33:46 +0900

    introduction-null-safety

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
 fun main(){
-    
+    var neverNull: String? = "This can't be null"
+
+//    neverNull = null
+
+    var nullable: String? = "You can keep a null here"
+
+    nullable = null
+
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    fun describeString(maybeString: String?): String {
+        if (maybeString != null && maybeString.length > 0) {
+            return "String of length ${maybeString.length}"
+        } else {
+            return "Empty or null string"
+        }
+    }
+
+    println(describeString(neverNull))
+    println(describeString(nullable))
+
+//    strLength(neverNull)
+//    strLength(nullable)
 }
