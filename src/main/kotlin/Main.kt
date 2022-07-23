ea0a8a3bf3c3bad3a839b27ece7f4edb7e7b27ba
wonjun <wonjun.choi@meshkorea.net>
2022-07-23 13:16:47 +0900

    introduction-functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,71 @@
-fun main(){
-    
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) { likedPeople.add(other) }  // 6
+}
+
+operator fun Int.times(str: String) = str.repeat(this)
+
+operator fun String.get(range: IntRange) = substring(range)
+
+fun main() {
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    println(sum(1, 2))
+    println(multiply(2, 4))
+
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "Bye ")
+
+    val pair = "Ferrari" to "Katrina"
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia
+
+    println(sophia.likedPeople.first().name)
+
+    println(2 * "Bye ")
+
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])
+
+    fun printAll(vararg messages: String) {
+        for (m in messages) println(m)
+    }
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好")
+
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Hola", "你好",
+        prefix = "Greeting: "
+    )
+
+    fun log(vararg entries: String) {
+        printAll(*entries) //spread operators
+    }
+
+    log("Hello", "Hallo", "Salut", "Hola", "你好", "123")
 }
