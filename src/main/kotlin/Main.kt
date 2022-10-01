5335f8c52db51bc371276fe83e0cf69b458a3aec
wonjun <wonjun.choi@meshkorea.net>
2022-10-01 20:02:58 +0900

    functional-extension-functions-and-properties

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,8 +10,33 @@ val Order.commaDelimitedItemNames: String                                       
 
 fun main() {
     val order = Order(listOf(Item("Bread", 25.0F), Item("Wine", 29.0F), Item("Water", 12.0F)))
+    val treeNode = TreeNode(5)
+
+    val result = mutableListOf<Int>()
+    result.add(treeNode.`val`)
 
     println("Max priced item name: ${order.maxPricedItemName()}")                           // 4
     println("Max priced item value: ${order.maxPricedItemValue()}")
     println("Items: ${order.commaDelimitedItemNames}")                                      // 5
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val result = mutableListOf<Int>()
+        inorder(root, result)
+        return result
+    }
+
+    fun inorder(root: TreeNode?, result: MutableList<Int>){
+        if(root != null){
+            inorder(root.left, result)
+            result.add(root.`val`)
+            inorder(root.right, result)
+        }
+    }
 }
